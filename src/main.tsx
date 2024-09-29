9fa3239b92f46b461d75a85eb0fbeed293b84ed5
Daniel Thompson <dthompson0190@gmail.com>
2024-09-29 17:54:21 -0500

    root name fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/customer-portal">
+  <BrowserRouter basename="/insurance-portal">
     <StrictMode>
       <App />
     </StrictMode>
