54153987b21d081b91d6ee7734f85524527016c6
Daniel Thompson <dthompson0190@gmail.com>
2024-10-02 09:38:39 -0500

    changed repo name

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/insurance-portal">
+  <BrowserRouter basename="/firebird">
     <StrictMode>
       <App />
     </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/insurance-portal",
+  base: "/firebird",
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
